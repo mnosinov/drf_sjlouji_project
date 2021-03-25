d6331a7e9cd48ea855142756f46437e90c5a0228
mnosinov <mnosinov@gmail.com>
2021-03-25 15:15:43 +0600

    list, update, delete apis have been added

--- employee/api.py
+++ employee/api.py
@@ -4,6 +4,24 @@ from .serializer import EmployeeSerializer
 from .models import Employee
 
 
+class EmployeeAPI(generics.ListAPIView):
+    serializer_class = EmployeeSerializer
+    # queryset = Employee.objects.all()
+
+    def get_queryset(self):
+        return Employee.objects.all()
+
+
 class EmployeeCreateAPI(generics.CreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeUpdateAPI(generics.RetrieveUpdateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class EmployeeDeleteAPI(generics.DestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .api import EmployeeCreateAPI
+from .api import EmployeeCreateAPI, EmployeeAPI, EmployeeUpdateAPI, EmployeeDeleteAPI
 
 
 urlpatterns = [
+    path('api', EmployeeAPI.as_view()),
     path('api/create', EmployeeCreateAPI.as_view()),
+    path('api/<int:pk>', EmployeeUpdateAPI.as_view()),
+    path('api/<int:pk>/delete', EmployeeDeleteAPI.as_view()),
 ]
