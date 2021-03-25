522faf02f38844c4bdcebdd1e07ddd2fc0a607d5
mnosinov <mnosinov@gmail.com>
2021-03-25 12:42:04 +0600

    another rest api hass been added. params as part of the urls - REST style

--- service/api.py
+++ service/api.py
@@ -2,7 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class HelloAPI(APIView):
+class Hello1API(APIView):
     def get(self, request, format=None):
         name = request.query_params.get('name')
+        return Response({'response': f'Hello, {name}!'})
+
+
+class Hello2API(APIView):
+    def get(self, request, format=None, **kwargs):
+        name = kwargs['name']
         return Response({'response': f'Hello, {name}!'})
--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .api import HelloAPI
+from .api import Hello1API, Hello2API
 
 urlpatterns = [
-    path('hello', HelloAPI.as_view()),
+    path('hello1', Hello1API.as_view()),
+    path('hello2/<str:name>', Hello2API.as_view()),
 ]
